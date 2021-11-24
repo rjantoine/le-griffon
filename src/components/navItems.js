8225c8be917eecbf74438ca7aa87d98f068211fd
rjantoine <rjantoine@gmail.com>
2021-11-24 08:56:40 -0500

    Ajouter Contact page contact

--- src/components/navItems.js
+++ src/components/navItems.js
@@ -3,7 +3,8 @@ const navItems = [
     {title:'À notre sujet', to:'/sujet/', key: 'nav-item-sujet'},
     {title:'Programmation', to:'/programme/', key: 'nav-item-programme'},
     {title:'Annonces', to:'/annonces/', key: 'nav-item-annonces'},
-    {title:'Activités', to:'/activites/', key: 'nav-item-activites'}
+    {title:'Activités', to:'/activites/', key: 'nav-item-activites'},
+    {title:'Nous joindre', to: '/contact/', key: 'nav-item-contact'},
 ]
 
 export default navItems

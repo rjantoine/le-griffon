ea0c7b58f733ddf64c159c574cb945869a655f24
rjantoine <rjantoine@gmail.com>
2021-12-06 22:20:06 -0500

    Fix spacing in Patrimoine

--- src/components/Patriomoine.js
+++ src/components/Patriomoine.js
@@ -3,7 +3,7 @@ import {Figure} from "react-bootstrap";
 import {StaticImage} from "gatsby-plugin-image";
 
 const Patrimoine = () => <div className="text-center mx-auto">
-    <Figure>
+    <Figure className="py-0">
         <StaticImage alt="Logo to Patrimoine Canada" src="./patrimoine-canadienne.jpg" />
         <Figure.Caption>Le Griffon vous offre cette soirée grâce aux subventions du Patrimoine Canadien.</Figure.Caption>
     </Figure>

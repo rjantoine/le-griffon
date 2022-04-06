cef8e02d8caa4fadd6842467a780027d151d3c75
rjantoine <rjantoine@gmail.com>
2022-04-06 13:25:10 -0400

    Enlevé DSG

--- src/templates/post.js
+++ src/templates/post.js
@@ -19,16 +19,6 @@ import {graphql} from "gatsby";
 //   }
 // `
 
-
-export async function config() {
-    // Optionally use GraphQL here
-    return ({ params }) => {
-        return {
-            defer: true,
-        }
-    }
-}
-
 const PurpleBorder = (props) => {
     const {data, pageContext, children} = props;
     console.log(props)

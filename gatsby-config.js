3338db56c1566dd346eef7f4b361a23a5931bcda
rjantoine <rjantoine@gmail.com>
2022-08-16 22:41:49 -0400

    Préciser la date de fin du Pavillon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
             options: {
               linkImagesToOriginal: false,
               disableBgImage: true,
+              maxWidth: 6000,
             },
           },
         ],

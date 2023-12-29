080b4e37891ffe1a965f8ffcca9b42b2d6390779
Robert Antoine <rob@musingmonkeys.com>
2023-12-28 19:42:52 -0500

    Ajouté: Quand le Père Noël revient au Club La Salle!

--- src/components/Patriomoine.js
+++ src/components/Patriomoine.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {Figure} from "react-bootstrap";
 import {StaticImage} from "gatsby-plugin-image";
 
-const Patrimoine = () => <div className="text-center mx-auto">
+const Patrimoine = ({msg="Le Griffon vous offre ceci grâce aux subventions du Patrimoine Canadien."}) => <div className="text-center mx-auto">
     <Figure className="py-0">
         <StaticImage alt="Logo to Patrimoine Canada" src="./patrimoine-canadienne.jpg" />
-        <Figure.Caption>Le Griffon vous offre ceci grâce aux subventions du Patrimoine Canadien.</Figure.Caption>
+        <Figure.Caption>{msg}</Figure.Caption>
     </Figure>
 </div>
 
